7e9bb63b512630da616557f39083b588209f0ed9
EC2 Default User <ec2-user@ip-172-31-85-177.ec2.internal>
2023-02-03 12:18:07 +0000

    [Finish] 3

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  # booksページ
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  # 保存機能
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  # 詳細画面
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  # 編集画面
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  # 更新
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
